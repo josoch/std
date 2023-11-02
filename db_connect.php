0cf98c070f691dc326410c964525816b7327239c
Joseph Ocholi <ocholijoseph@gmail.com>
2023-11-02 09:29:07 +0100

    conect

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','payroll')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','skydigi_payroll','gNNBZ%I60K','skydigi_payroll')or die("Could not connect to mysql".mysqli_error($con));
